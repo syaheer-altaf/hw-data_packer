e632a7d140f53297c0181db5be5c943c989f94e0
syaheer-altaf <altafsyaheer@gmail.com>
2025-03-01 20:39:12 +0800

    homework completed: both pack and unpack methods are correctly implemented

--- short_string_packer.rb
+++ short_string_packer.rb
@@ -7,7 +7,10 @@ module ShortStringPacker
   #   str - String object
   # Returns: a Integer object
   def self.pack(str)
-    # IMPLEMENT THIS METHOD
+    n_bit_shift = Math.log2(26).ceil
+    str.chars.reduce(0) do |num, alphabet|
+      (num << n_bit_shift) | (alphabet.ord - 'a'.ord + 1)
+    end
   end
 
   ## Unpacks a Integer from pack() method into a short string
@@ -15,6 +18,8 @@ module ShortStringPacker
   #   packed - a Integer object
   # Returns: a String object
   def self.unpack(packed)
-    # IMPLEMENT THIS METHOD
+    n_bit_shift = Math.log2(26).ceil
+    masked_values = packed.digits(2**n_bit_shift).reverse
+    masked_values.map { |n| (n + 'a'.ord - 1).chr }.join
   end
 end
